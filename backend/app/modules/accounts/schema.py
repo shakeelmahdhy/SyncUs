aba82dddc0d604d65025d07cfce4c5347d15f569
serene-philip <serene.philip44@gmail.com>
2026-05-03 19:19:27 +1000

    Add UserUpdateRequest for user data updates
    Added UserUpdateRequest class for optional user fields.

--- backend/app/modules/accounts/schema.py
+++ backend/app/modules/accounts/schema.py
@@ -1,5 +1,30 @@
 from pydantic import BaseModel
+from uuid import UUID
 
-class UserCreate(BaseModel):
+
+# -------- USER --------
+class UserCreateRequest(BaseModel):
     name: str
     email: str
+
+
+class UserResponse(BaseModel):
+    id: UUID
+    name: str
+    email: str
+
+
+class UserUpdateRequest(BaseModel):
+    name: Optional[str] | None = None
+    email: Optional[str] | None = None
+
+
+# -------- RESUME --------
+class ResumeCreateRequest(BaseModel):
+    file_url: str
+
+
+class ResumeResponse(BaseModel):
+    id: UUID
+    file_url: str
+    user_id: UUID
