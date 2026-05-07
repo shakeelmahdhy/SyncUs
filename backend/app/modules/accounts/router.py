896836cf3e1404c5dade6a21ae9ca98d3526f9f3
serene-philip <serene.philip44@gmail.com>
2026-05-07 14:37:01 +1000

    Fix import statements in router.py

--- backend/app/modules/accounts/router.py
+++ backend/app/modules/accounts/router.py
@@ -7,8 +7,8 @@ from .schema import (
     UserResponse,
     UserUpdateRequest,
     ResumeCreateRequest,
-    ResumeResponse
-    RegisterRequest
+    ResumeResponse,
+    RegisterRequest,
     LoginRequest
 )
 
